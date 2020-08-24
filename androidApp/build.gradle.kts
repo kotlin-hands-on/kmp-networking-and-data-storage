e2cd12f67cf64a8299cbb1bb4fc83a26d23a09ae
Ekaterina Petrova <91kathrin@gmail.com>
2020-08-24 22:36:25 +0300

    step-08: implement Android application

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -8,6 +8,12 @@ dependencies {
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0-native-mt")
+    implementation("androidx.core:core-ktx:1.3.1")
+    implementation("androidx.recyclerview:recyclerview:1.1.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
 
 android {
@@ -24,4 +30,13 @@ android {
             isMinifyEnabled = false
         }
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
 }
+
