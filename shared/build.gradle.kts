26fc388de4372eced77db7a6153bd6392cc8dc6e
hlnstepanova <hlnstepanova@yandex.ru>
2020-10-16 15:30:00 +0100

    workaround for native database driver not found on ios

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -19,6 +19,12 @@ repositories {
 }
 kotlin {
     android()
+    val onPhone = System.getenv("SDK_NAME")?.startsWith("iphoneos") ?: false
+    if (onPhone) {
+        iosArm64("ios")
+    } else {
+        iosX64("ios")
+    }
     ios {
         binaries {
             framework {
