84a8ccaefb9af8e03219bce6d762a875c46d2a1e
Pamela Hill <pamela.a.hill@gmail.com>
2022-12-14 10:11:39 +0200

    Update build.gradle.kts
    Co-authored-by: Danil Pavlov <danil.pavlov@jetbrains.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
         classpath("com.android.tools.build:gradle:7.2.2")
     }
 }
