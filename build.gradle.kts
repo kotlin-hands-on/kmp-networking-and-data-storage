5eed4cefe8d115028553aca596848f44e5b6bb26
Pamela Hill <pamela.a.hill@gmail.com>
2022-12-14 10:11:11 +0200

    Update build.gradle.kts
    Co-authored-by: Danil Pavlov <danil.pavlov@jetbrains.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ buildscript {
     val sqlDelightVersion = "1.5.3"
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.22")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
         classpath("com.squareup.sqldelight:gradle-plugin:$sqlDelightVersion")
         classpath("com.android.tools.build:gradle:7.2.2")
     }
