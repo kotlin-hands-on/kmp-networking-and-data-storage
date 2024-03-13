e5b2b8d947bc87bde106c9beff2942a191db40cf
pahill <pamela.a.hill@gmail.com>
2024-03-13 08:34:45 +0100

    Aleksey - @Throws readd

--- shared/src/commonMain/kotlin/com/jetbrains/spacetutorial/SpaceXSDK.kt
+++ shared/src/commonMain/kotlin/com/jetbrains/spacetutorial/SpaceXSDK.kt
@@ -8,6 +8,7 @@ import com.jetbrains.spacetutorial.network.SpaceXApi
 class SpaceXSDK(databaseDriverFactory: DatabaseDriverFactory, val api: SpaceXApi) {
     private val database = Database(databaseDriverFactory)
 
+    @Throws(Exception::class)
     suspend fun getLaunches(forceReload: Boolean): List<RocketLaunch> {
         val cachedLaunches = database.getAllLaunches()
         return if (cachedLaunches.isNotEmpty() && !forceReload) {
