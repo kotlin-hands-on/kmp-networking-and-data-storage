930cded508b31db02417ca75f71332b6eee1ade9
pahill <pamela.a.hill@gmail.com>
2025-06-30 15:59:05 +0200

    Update deps for June 2025

--- shared/src/androidMain/kotlin/com/jetbrains/spacetutorial/Platform.android.kt
+++ shared/src/androidMain/kotlin/com/jetbrains/spacetutorial/Platform.android.kt
@@ -1,3 +1,5 @@
+package com.jetbrains.spacetutorial
+
 import android.os.Build
 
 class AndroidPlatform : Platform {
--- shared/src/iosMain/kotlin/com/jetbrains/spacetutorial/Platform.ios.kt
+++ shared/src/iosMain/kotlin/com/jetbrains/spacetutorial/Platform.ios.kt
@@ -1,6 +1,8 @@
+package com.jetbrains.spacetutorial
+
 import platform.UIKit.UIDevice
 
-class IOSPlatform: Platform {
+class IOSPlatform : Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
