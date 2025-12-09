13ddc940a6151ca6a5745cd9c4714acdae7727bd
Arnaud Giuliani <giuliani.arnaud@gmail.com>
2025-12-09 17:58:21 +0100

    Use common KoinApp entry point

--- composeApp/src/androidMain/kotlin/com/jetbrains/spacetutorial/MainApplication.kt
+++ composeApp/src/androidMain/kotlin/com/jetbrains/spacetutorial/MainApplication.kt
@@ -1,12 +1,18 @@
 package com.jetbrains.spacetutorial
 
 import android.app.Application
-import com.jetbrains.spacetutorial.di.AndroidKoinApp
+import com.jetbrains.spacetutorial.di.KoinApp
+import org.koin.android.ext.koin.androidContext
+import org.koin.ksp.generated.startKoin
 
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        AndroidKoinApp.initKoin(this)
+        // use startKoin generated in KoinApp (thanks to @KoinApplication)
+        // allow to pass extra arguments like androidContext
+        KoinApp.startKoin {
+            androidContext(this@MainApplication)
+        }
     }
 }
