fc2e19f37e59facccc5eeff01896030694ae40b1
pahill <pamela.a.hill@gmail.com>
2026-02-10 14:30:55 +0200

    Small fixes after warnings when deploying

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 kotlin {
     androidLibrary {
-        namespace = "com.jetbrains.spacetutorial"
+        namespace = "com.jetbrains.spacetutorial.android"
         compileSdk = libs.versions.android.compileSdk.get().toInt()
         minSdk = libs.versions.android.minSdk.get().toInt()
 
